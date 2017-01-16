72557a0bdb47827b52e22655ab61b9d749164d67
AsuriV <AsuriV@NJN0SK22.sqs.group.intl>
2017-01-16 14:00:32 -0500

    First commit; login to gmail successfully; does not load inbox, instead loads previous page.

---
+++ gmailLogin/src/main/java/AutomationTrainingPrj1/gmailLogin/App.java
@@ -0,0 +1,13 @@
+package AutomationTrainingPrj1.gmailLogin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gmailLogin/src/test/java/AutomationTrainingPrj1/gmailLogin/gmailLoginTest.java
@@ -0,0 +1,78 @@
+package AutomationTrainingPrj1.gmailLogin;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class gmailLoginTest {
+  @Test
+  public void f() {
+	  File file = new File("C:/Users/asuriv/SQS-Training/SeleniumTraining/chromedriver/chromedriver.exe");
+	  System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+      // Create the Chrome driver object.
+      WebDriver driver = new ChromeDriver();
+	  
+//	  File file = new File("C:/Users/asuriv/SQS-Training/SeleniumTraining/geckodriver/geckodriver.exe");
+//	  System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+//
+//      WebDriver driver = new FirefoxDriver();
+
+
+      // And use it to visit Google
+      //driver.get("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/#identifier");
+      driver.get("http://mail.google.com");
+      
+      //driver.manage().window().maximize(); // maximize window.
+      
+      // delete cookies (& clear cache?)
+      driver.manage().deleteAllCookies();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      // get the email input text box.
+      WebElement emailInput = driver.findElement(By.id("Email"));
+      emailInput.sendKeys("vid.auto.test@gmail.com");
+      
+      
+      // grab the next button by ID
+      WebElement nextButton = driver.findElement(By.id("next"));
+      // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
+      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      nextButton.click();
+      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      WebElement passwordInput = driver.findElement(By.id("Passwd")); // #Passwd password-shown
+      passwordInput.sendKeys("piOctIj8");
+
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      
+      // grab the next button by ID
+      WebElement signInBtn = driver.findElement(By.id("signIn"));
+      driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
+      signInBtn.click();
+      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+      driver.close();
+      driver.quit();
+
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
