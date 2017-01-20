b092b570272cf81a9bdb6e7c6c4196777ea3564c
AsuriV <AsuriV@NJN0SK22.sqs.group.intl>
2017-01-20 12:51:12 -0500

    for marbles.shopvisible.com, create test case to verify that if an item is not backordered, then available quantity is greater than 0.Also get the value for the part number and product number; use assertion exceptions to verify that product quantity available is greater than 0 if item is not backordered.

--- gmailLogin/src/test/java/AutomationTrainingPrj1/gmailLogin/BookingTest.java
+++ gmailLogin/src/test/java/AutomationTrainingPrj1/gmailLogin/BookingTest.java
@@ -6,6 +6,10 @@ import java.awt.AWTException;
 //import java.awt.Robot;
 //import java.awt.event.KeyEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,14 +23,13 @@ import org.testng.annotations.BeforeMethod;
 
 public class BookingTest {
   @Test
-  public void f() throws AWTException {
+  public void testSearch() throws AWTException {
 	  File file = new File("C:/Users/asuriv/SQS-Training/SeleniumTraining/chromedriver/chromedriver.exe");
 	  System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
       // Create the Chrome driver object.
       WebDriver driver = new ChromeDriver();
       driver.get("http://marbles.shopvisible.com/");
-//      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);// give it a minute to finish loading.
       
       // enter destination in web element with id = ss
       driver.manage().deleteAllCookies();
@@ -34,32 +37,75 @@ public class BookingTest {
       
       WebDriverWait wait = new WebDriverWait(driver,20);
       WebElement searchInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("tSearch")));
-      //WebElement popOverXBtn = driver.findElement(By.xpath("//*[@id=\"newsletter-box\"]/div"));
       WebElement popOverXBtn = driver.findElement(By.className("cl"));
       
       popOverXBtn.click();
       
-    		//*[@id="newsletter-box"]/div
       searchInput.sendKeys("scrabble");
       searchInput.sendKeys(Keys.ENTER);
-      //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);// 5 seconds to finish typing.
+      
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// give it 1 minute to finish loading results page.
+      List<Float> listOfPrices = new ArrayList<Float>();
+      
+      List<WebElement>  listOfProductPrices = driver.findElements(By.xpath("//div[@class='prod-list']/div[@class='prod-item']/div[@class='prod-item-box']/div[@class='inf']/p[@class='price']")); //@id='b1'
+      for(int i=0; i < listOfProductPrices.size(); i++){
+    	  List<WebElement> we = listOfProductPrices.get(i).findElements(By.tagName("span")); // span elements within the p tags with class=price contains the actual prices.
+    	  // span elements are found only when there is a sale. If not, price is located in the p element with class=price
+    	  int lenWe = we.size();
+    	  if(lenWe > 1){
+    		  // item is on sale, since a span element was found.
+    		  // select span element with class='was'
+    		  // there can only be original price and sale price, so it's safe to assume that there will only be on
+    		  // element with class = 'was'; same logic applies for sale price.
+    		  WebElement originalPrice = driver.findElement(By.xpath("//div[@class='prod-list']/div[@class='prod-item']/div[@class='prod-item-box']/div[@class='inf']/p[@class='price']/span[@class='was']"));
+    		  WebElement pTagClassPrice = driver.findElement(By.xpath("//div[@class='prod-list']/div[@class='prod-item']/div[@class='prod-item-box']/div[@class='inf']/p[@class='price']"));
+    		  String isItemBackOrdered = pTagClassPrice.getAttribute("data-bo");
+    		  int numAvailable = Integer.parseInt(pTagClassPrice.getAttribute("data-qty"));
+    		  if(originalPrice != null){
+    			  String priceString = originalPrice.getText(); // priceString contains '$' as the first character.
+    			  //System.out.println("Original price is " + priceString);
+    		  }
+    		  WebElement salePrice = driver.findElement(By.xpath("//div[@class='prod-list']/div[@class='prod-item']/div[@class='prod-item-box']/div[@class='inf']/p[@class='price']/span[@class='sale']"));
+    		  if(salePrice != null){
+    			  String priceString = salePrice.getText(); // priceString contains '$' as the first character.
+    			  System.out.println("Sale price is " + priceString);
+    			  listOfPrices.add(Float.parseFloat(priceString.substring(1,priceString.length()))); // sale price gets added to the list of prices.
+    			  System.out.println(numAvailable + " no(s) available at " + priceString + " each");
+    		  }
+			  if(!Boolean.parseBoolean(isItemBackOrdered)){
+				  // item is not out of stock, verify item quantity must be greater than 0.
+				  throw new AssertionError(numAvailable > 0);
+			  }
 
-      // press enter in the search box
-//      Robot r = new Robot();
-//      r.keyPress(KeyEvent.VK_ENTER);
-//      r.keyRelease(KeyEvent.VK_ENTER);
-      
-      driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);// give it 1 minute to finish loading results page.
-      // get number of elements with class - prod-item. This is the number of resuts.
-      WebElement results = driver.findElement(By.className("prod-item"));
-      
+    		  //<p class="price" data-prod="6985" data-partno="11670" data-dept="152" data-qty="26" data-bo="False"><span class="was">$59.99</span><span class="sale">$29.99</span></p>
+			  int prodNumber = Integer.parseInt(pTagClassPrice.getAttribute("data-prod"));
+			  int partNumber = Integer.parseInt(pTagClassPrice.getAttribute("data-partno"));
+			  
+			  
+			  
+			  
+    	  }
+    	  else{
+			  String priceString = listOfProductPrices.get(i).getText(); // priceString contains '$' as the first character.
+			  listOfPrices.add(Float.parseFloat(priceString.substring(1,priceString.length()))); // no sale price, so add the only price listed for the product.
+			  String backOrder = listOfProductPrices.get(i).getAttribute("data-bo");
+			  int numAvailable = Integer.parseInt(listOfProductPrices.get(i).getAttribute("data-qty"));
+			  System.out.println(numAvailable + " no(s) available at " + priceString + " each");
+			  if(!Boolean.parseBoolean(backOrder)){
+				  // item is not out of stock, verify item quantity must be greater than 0.
+				  throw new AssertionError(numAvailable > 0);
+			  }
+    	  }
+      }
+      // sort the list of prices.
+      Collections.sort(listOfPrices); //
+      // just click on the first result for now, to proceed to buy the toy.
+      // get the lowest price and index
+      driver.close();
+      driver.quit();
 	  // XPath for the close (x) button on the pop-over. //*[@id="newsletter-box"]/div 
-      //*[@id="content-wrapper"]/main/div[3]/div[2]/div[2]/div[1]/a
-      //*[@id="content-wrapper"]/main/div[3]/div[2]/div[2]/div[2]/a
-      
-     // xpath = //*[@id="content-wrapper"]/main/div[3]/div[2]/div[2]/div[2]/a
-      
-  }
+   }
+  
   @BeforeMethod
   public void beforeMethod() {
   }
--- gmailLogin/src/test/java/AutomationTrainingPrj1/gmailLogin/gmailLoginTest.java
+++ gmailLogin/src/test/java/AutomationTrainingPrj1/gmailLogin/gmailLoginTest.java
@@ -1,21 +1,28 @@
 package AutomationTrainingPrj1.gmailLogin;
 
 import org.testng.annotations.Test;
+
+
+
 import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
+// XPath tutorial - http://zvon.org/xxl/XPathTutorial/Output/examples.html
 public class gmailLoginTest {
 
   @Test
@@ -32,19 +39,19 @@ public class gmailLoginTest {
       // get the email input text box.
       WebElement emailInput = driver.findElement(By.id("Email"));
       emailInput.sendKeys("vid.auto.test@gmail.com"); // enter email.
-      
-      // grab the next button by ID
-      WebElement nextButton = driver.findElement(By.id("next"));
       // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
       driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      // grab the next button by ID
+      WebElement nextButton = driver.findElement(By.id("next"));
+
       nextButton.click();
-      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
       WebElement passwordInput = driver.findElement(By.id("Passwd")); // #Passwd password-shown
       passwordInput.sendKeys("piOctI$*"); // enter incorrect password.
 
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
       
-      // grab the next button by ID
+      // grab the sign-in button by ID
       WebElement signInBtn = driver.findElement(By.id("signIn"));
       driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
       // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
@@ -53,10 +60,74 @@ public class gmailLoginTest {
       
       // verify there is an error message since an incorrect password was entered.
       // xpath for the error message span element. //*[@id="errormsg_0_Passwd"]
-//      WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"errormsg_0_Passwd\"]"));
+      WebDriverWait wait = new WebDriverWait(driver, 10);
+      WebElement errorAlert = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@id='errormsg_0_Passwd']")));
+
       
-    WebElement errorMessage = driver.findElement(By.id("errormsg_0_Passwd"));
+      //WebElement errorMessage = driver.findElement(By.xpath("//span[@id='errormsg_0_Passwd']"));
+      
+      
+      System.out.println(errorAlert.getText());
+//      WebElement errorMessage = driver.findElement(By.id("errormsg_0_Passwd"));
+      driver.close();
+      driver.quit();
   }
+  
+  @Test
+  public void validLoginToYahoo() {
+	  //login-username MeadGotHech2 
+	  // pwd = vid_auto_test@yahoo.com/starpOmgeed3
+	  File file = new File("C:/Users/asuriv/SQS-Training/SeleniumTraining/geckodriver/geckodriver.exe");
+	  System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+
+      WebDriver driver = new FirefoxDriver();
+
+      driver.get("https://login.yahoo.com/?.src=ym&.intl=us&.lang=en-US&.done=https%3a//mail.yahoo.com");
+      Set<Cookie> cookies = driver.manage().getCookies();
+      Iterator<Cookie> ci = cookies.iterator();
+      while(ci.hasNext()){
+    	  Cookie c = ci.next();
+    	  driver.manage().deleteCookie(c);
+      }
+      
+      
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      // get the email input text box.
+      WebElement emailInput = driver.findElement(By.id("login-username"));
+      emailInput.sendKeys("vid_auto_test"); // enter email.
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      
+      // uncheck "Stay signed in " checkbox
+      WebElement staySignedInChkBox = driver.findElement(By.xpath("//*[@id=\"persistency\"]/div/label"));
+      staySignedInChkBox.click();
+      
+//      if(staySignedInChkBox.isDisplayed() && staySignedInChkBox.isEnabled() && staySignedInChkBox.isSelected()){
+//    	  staySignedInChkBox.click();
+//      }
+      
+      // grab the next button by ID
+      WebElement nextButton = driver.findElement(By.id("login-signin"));
+      // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
+      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      nextButton.click();
+      driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+      WebElement passwordInput = driver.findElement(By.id("login-passwd")); 
+      passwordInput.sendKeys("starpOmgeed3"); // enter correct password.
+
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      
+      // grab the next button by ID
+      WebElement signInBtn = driver.findElement(By.id("login-signin"));
+      driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
+      signInBtn.click();
+      driver.close();
+      driver.quit();
+
+
+	  
+  }  
+  
   
   @Test
   public void invalidEmail() {
@@ -75,14 +146,15 @@ public class gmailLoginTest {
       
       // grab the next button by ID
       WebElement nextButton = driver.findElement(By.id("next"));
-      // http://toolsqa.com/selenium-webdriver/findelement-and-findelements-command/
-      //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
       nextButton.click();
       
       // Now expect an error message "Please enter a valid email address." to be displayed.
       WebElement errorMessage = driver.findElement(By.id("errormsg_0_Email")); 
 
-      System.out.println("Error message " +errorMessage.getText());
+      System.out.println("Error message " + errorMessage.getText());
+      driver.close();
+      driver.quit();
+
       
   }
 	
